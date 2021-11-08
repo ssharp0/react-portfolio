7fc701e99590c42f67d86842445b6083ed081ed8
SHARP <ssharp9999@gmail.com>
2021-11-08 12:11:36 -0800

    about data

---
+++ src/components/AboutSection/AboutData.js
@@ -0,0 +1,4 @@
+export const aboutData = {
+ heading: 'About Me',
+ aboutDesc: 'I am a Southern California resident and love to go hiking with my partner Emily and my two amazing pups. I have a team oriented personality and love working with others to build exciting applications and implement new technologies'
+}
