329675488328812c62c30a9aba20f22de27d5d09
SHARP <ssharp9999@gmail.com>
2021-11-08 14:48:40 -0800

    tech data and icons

---
+++ src/components/TechnologiesSection/TechnologiesData.js
@@ -0,0 +1,76 @@
+import {
+ FaHtml5,
+ FaCss3Alt,
+ FaJs,
+ FaNodeJs,
+ FaPython,
+ FaReact,
+ FaNpm,
+ FaGitAlt,
+ FaBootstrap
+} from 'react-icons/fa'
+
+import {
+ SiMongodb,
+ SiHeroku
+} from 'react-icons/si'
+
+import {
+ DiMysql
+} from 'react-icons/di'
+
+export const iconList = [
+ {
+  icon: <FaHtml5 />,
+  desc: 'HTML'
+ },
+ {
+  icon: <FaCss3Alt />,
+  desc: 'CSS'
+ },
+ {
+  icon: <FaJs />,
+  desc: 'Javascript'
+ },
+ {
+  icon: <FaPython />,
+  desc: 'Python'
+ },
+ {
+  icon: <FaReact />,
+  desc: 'React'
+ },
+ {
+  icon: <DiMysql />,
+  desc: 'MySQL'
+ },
+ {
+  icon: <SiMongodb />,
+  desc: 'MongoDB'
+ },
+ {
+  icon: <FaNpm />,
+  desc: 'npm'
+ },
+ {
+  icon: <FaNodeJs />,
+  desc: 'NodeJS'
+ },
+ {
+  icon: <FaGitAlt />,
+  desc: 'Git'
+ },
+ {
+  icon: <SiHeroku />,
+  desc: 'Heroku'
+ },
+ {
+  icon: <FaBootstrap />,
+  desc: 'Bootstrap'
+ },
+]
+
+export const TechnologiesData = {
+ heading: 'Technology Highlights',
+ technologiesDesc: 'Some of the technologies I work with and enjoy'
+}
