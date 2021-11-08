2daf1f640075bda187079c3ba2040976429cdd3e
SHARP <ssharp9999@gmail.com>
2021-11-08 14:48:20 -0800

    tech layout and styling

--- src/components/TechnologiesSection/TechnologiesElements.js
+++ src/components/TechnologiesSection/TechnologiesElements.js
@@ -29,5 +29,87 @@ export const TechnologiesRow = styled.div`
  grid-template-areas: 'column2 column1'
 `
 
+export const TechnologiesColumn1 = styled.div`
+ text-align: center;
+ padding: 10px;
+ margin-bottom: 10px;
+ grid-area: column1;
+`
 
+export const TechnologiesColumn2 = styled.div`
+ text-align: center;
+ padding: 10px;
+ margin-bottom: 10px;
+ grid-area: column2;
+`
+
+export const TechnologiesTextWrapper = styled.div`
+padding-top: 0;
+padding-bottom: 50px;
+max-width: 550px;
+`
+export const TechnologiesHeading = styled.h1`
+ margin-bottom: 25px;
+ font-size: 45px;
+ line-height: 1;
+ font-weight: 750;
+ text-transform: uppercase;
+ color: #fff;
+
+ @media screen and (max-width: 480px) {
+  font-size: 30px;
+ }
+`
+
+export const TechnologiesDescription = styled.p`
+ max-width: 430px;
+ margin-bottom: 30px;
+ font-size: 19px;
+ line-height: 25px;
+ color: #fff;
+
+ @media screen and (max-width: 480px) {
+  font-size: 15px;
+ }
+`
+
+export const BtnWrap = styled.div`
+ display: flex;
+ justify-content: flex-start;
+`
+
+export const TechnologiesColumns = styled.div`
+ display: grid;
+ grid-template-columns: repeat(4, 1fr);
+
+ @media screen and (max-width: 480px) {
+ grid-template-columns: repeat(2, 1fr);
+}
+`
+
+export const TechnologiesColummItem = styled.div`
+ font-size: 25px;
+ margin-bottom: 35px;
+ color: #fff;
+ 
+ @media screen and (max-width: 480px) {
+ font-size: 10px;
+ margin-bottom: 10px;
+}
+`
+
+export const TechnologiesColumnBadge = styled.div`
+ font-size: 50px;
+ margin-bottom: 5px;
+ color: #fff;
+
+ &:hover {
+  color: #A56336;
+ }
+
+ @media screen and (max-width: 480px) {
+ font-size: 20px;
+ margin-bottom: 2px;
+}
+`
 
--- src/components/TechnologiesSection/index.js
+++ src/components/TechnologiesSection/index.js
@@ -4,7 +4,13 @@ import {
  TechnologiesContainer, 
  TechnologiesWrapper, 
  TechnologiesRow, 
-
+ TechnologiesColumn1, 
+ TechnologiesColumn2, 
+ TechnologiesTextWrapper, 
+ TechnologiesHeading, 
+ TechnologiesDescription, 
+ TechnologiesColummItem, 
+ TechnologiesColumnBadge 
 } from './TechnologiesElements'
 
 import { iconList, TechnologiesData } from './TechnologiesData'
@@ -20,7 +26,28 @@ const TechnologiesSection = () => {
    <TechnologiesContainer id='technologies'>
     <TechnologiesWrapper>
      <TechnologiesRow>
-
+      <TechnologiesColumn1>
+       <TechnologiesTextWrapper>
+        <TechnologiesHeading>{TechnologiesData.heading}</TechnologiesHeading>
+        <hr />
+        <br />
+        <TechnologiesDescription>{TechnologiesData.technologiesDesc}</TechnologiesDescription>
+       </TechnologiesTextWrapper>
+      </TechnologiesColumn1>
+      <TechnologiesColumn2 style={{marginRight: '10px'}}>
+       <Paper elevation={5} style={{backgroundColor: '#263C41'}}>
+       <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+         {iconList.map((icon, index) => (
+          <Grid item xs={3} sm={4} md={4} key={index}>
+           <TechnologiesColumnBadge>{icon.icon}</TechnologiesColumnBadge>
+           <TechnologiesColummItem>{icon.desc}</TechnologiesColummItem>
+          </Grid>
+         ))}
+        </Grid>
+       </Box>
+       </Paper>
+      </TechnologiesColumn2>
      </TechnologiesRow>
     </TechnologiesWrapper>
    </TechnologiesContainer>
