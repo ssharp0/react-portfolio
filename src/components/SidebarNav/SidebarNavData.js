c1443565d654ca8e4a9142487e89cd678955f0db
SHARP <ssharp9999@gmail.com>
2021-11-08 12:13:27 -0800

    sidenavbar data

---
+++ src/components/SidebarNav/SidebarNavData.js
@@ -0,0 +1,18 @@
+export const SidebarNavData = [
+ {
+  section: 'about',
+  label: 'About Me'
+ },
+ {
+  section: 'technologies',
+  label: 'Technologies'
+ },
+ {
+  section: 'projects',
+  label: 'Projects'
+ },
+ {
+  section: 'contact',
+  label: 'Contact'
+ },
+]
