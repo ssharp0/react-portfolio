29e0994e166a02d989570edddb90607f244e1144
SHARP <ssharp9999@gmail.com>
2021-11-08 13:55:11 -0800

    about section layout

---
+++ src/components/AboutSection/AboutElements.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const AboutContainer = styled.div`
+ background: #A56336;
+ color: #fff;
+
+ @media screen and (max-width: 768px) {
+  padding: 90px 0;
+ }
+`
+
+export const AboutWrapper = styled.div`
+ height: 850px;
+ width: 100%;
+ max-width: 1000px;
+ justify-content: center;
+ display: grid;
+ z-index: 1;
+ margin-right: auto;
+ margin-left: auto;
+ padding: 25px;
+`
+
+export const AboutImgWrap = styled.div`
+ text-align: center;
+ max-width: 555px;
+ height: 100%;
+`
+
+export const AboutImg = styled.img`
+ width: 85%;
+ margin-right: 1%;
+ margin-left: 1%;
+ margin-top: 5%;
+ margin-bottom: 5%;
+ border-radius: 10px;
+ opacity: 75%;
+`
+
+export const AboutRow = styled.div`
+ align-items: center;
+ display: grid;
+ grid-auto-columns: minmax(auto, 1fr);
+ grid-template-areas: 'column1 column2';
+`
+
+
+
---
+++ src/components/AboutSection/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { 
+ AboutContainer, 
+ AboutWrapper, 
+ AboutRow, 
+
+} from './AboutElements'
+
+import { aboutData } from './AboutData';
+
+import SelfImg from '../../images/self.jpeg'
+
+import Paper from '@mui/material/Paper';
+
+const AboutSection = () => {
+
+ return (
+  <>
+   <AboutContainer id='about'>
+    <AboutWrapper>
+     <AboutRow>
+
+     </AboutRow>
+    </AboutWrapper>
+   </AboutContainer>
+  </>
+ )
+}
+
+export default AboutSection
