e893d0f25ceb7228416b0a9f00156ea081048379
SHARP <ssharp9999@gmail.com>
2021-11-08 15:35:42 -0800

    contact layout

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -4,7 +4,11 @@ import {
  ContactContainer, 
  ContactWrapper, 
  ContactRow, 
-
+ ContactColumn1, 
+ ContactColumn2, 
+ ContactTextWrapper, 
+ ContactHeading, 
+ ContactImgWrap, 
 } from './ContactElements'
 
 import ContactPageIcon from '@mui/icons-material/ContactPage';
@@ -20,7 +24,32 @@ const ContactSection = () => {
    <ContactContainer id='contact'>
     <ContactWrapper>
      <ContactRow>
-
+      <ContactColumn1>
+       <ContactTextWrapper>
+        <ContactHeading>{contactData.heading}</ContactHeading>
+        <hr />
+        <br />
+        {contactDetails.map(contact => {
+         return <Accordion elevation={8} className='contactItem'>
+          <AccordionSummary expandIcon={contact.contactIcon} aria-controls="panel1a-content" id="panel1a-header">
+           <Typography style={{ padding: '1%', color: '#143642' }} ><strong>{contact.method}</strong></Typography>
+          </AccordionSummary>
+           <AccordionDetails>
+            <Typography>
+             {contact.method !== 'Email' ? <Button style={{ backgroundColor: '#A56336' }} variant='contained' href={contact.path} target="_blank">Go To {contact.name}</Button> : <h5 style={{ color: '#A56336'}} >{contact.path}</h5>}
+            </Typography>
+           </AccordionDetails>
+         </Accordion>
+        })}
+       </ContactTextWrapper>
+      </ContactColumn1>
+      <ContactColumn2>
+       <ContactImgWrap>
+        <Paper elevation={5} style={{ height: '100%', width: '100%', backgroundColor: '#263C41'}}>
+         <ContactPageIcon style={{ height: '100%', width: '100%', color: 'white' }} />
+        </Paper>
+       </ContactImgWrap>
+      </ContactColumn2>
      </ContactRow>
     </ContactWrapper>
    </ContactContainer>
