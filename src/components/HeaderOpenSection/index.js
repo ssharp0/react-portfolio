e6633b6a5e266fc284ce72a394ac72dcf2fafaa5
SHARP <ssharp9999@gmail.com>
2021-11-08 17:20:01 -0800

    header open section elements

--- src/components/HeaderOpenSection/index.js
+++ src/components/HeaderOpenSection/index.js
@@ -25,7 +25,23 @@ const HeaderOpenSection = () => {
 
  return (
   <HeaderOpenContainer>
-
+   <HeaderOpenBg>
+    <ParticlesBackGround />
+   </HeaderOpenBg>
+   <HeaderOpenContent>
+    <HeaderOpenHeading>
+     <Typed strings={["Hello, I'm Scott."]} typeSpeed={75} />
+    </HeaderOpenHeading>
+    <HeaderOpenBtnWrapper>
+     <GoContactButton to="contact" smooth={true} duration={500} spy={true} exact="true" onMouseEnter={onHover} onMouseLeave={onHover}>
+      <Typed strings={['Say Hello!']} typeSpeed={75} />
+      {hover ? <ContactInfoIcon /> : <HelloIcon />}
+     </GoContactButton>
+    </HeaderOpenBtnWrapper>
+    <HeaderOpenSubHeading>
+     <Typed strings={['Web Development', 'Full Stack Software Engineering', 'MERN']} typeSpeed={70} backSpeed={90} loop />
+    </HeaderOpenSubHeading>
+   </HeaderOpenContent>
   </HeaderOpenContainer>
  )
 }
