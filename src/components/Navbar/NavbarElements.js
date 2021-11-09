45efec9524cc67276974defdc400b0331c85c976
SHARP <ssharp9999@gmail.com>
2021-11-08 17:07:00 -0800

    nav setup

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components'
+import { Link as LinkRouter } from 'react-router-dom'
+import { Link as LinkScroll } from 'react-scroll'
+
+export const NavSection = styled.nav`
+ background: ${({ positionScrollNav }) => (positionScrollNav ? '#A56336' : 'transparent')};
+ margin-top: -65px;
+ height: 65px;
+ display: flex;
+ justify-content: center;
+ font-size: 1rem;
+ top: 0;
+ z-index: 10;
+ position: sticky;
+
+ @media screen and (max-width: 960px) {
+  transition: 0.5s all ease;
+ }
+`
+
+export const NavContainer = styled.div`
+ width: 100%;
+ padding: 25px;
+ max-width: 1000px;
+ justify-content: space-between;
+ display: flex;
+ height: 65px;
+ z-index: 1;
+`
+
+export const NavLabel = styled(LinkRouter)`
+ color: #fff;
+ cursor: pointer;
+ text-decoration: none;
+ font-size: 1.5rem;
+ display: flex;
+ align-items: center;
+ margin-left: 20px;
+ font-weight: bold;
+
+ &:hover {
+ font-size: 2rem;
+}
+`
+
+export const NavScrollLinks = styled(LinkScroll)`
+ color: #fff;
+ display: flex;
+ align-items: center;
+ text-decoration: none;
+ padding: 1rem;
+ height: 100%;
+ cursor: pointer;
+ 
+ &:hover {
+  font-size: 1.5rem;
+ }
+
+ &.active {
+  border-bottom: 4px solid #fff;
+  color: #143642;
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-transform: uppercase;
+ }
+
+`
