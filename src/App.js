77a136da3dccbb152d934ab71016af45de462028
SHARP <ssharp9999@gmail.com>
2021-11-08 17:25:52 -0800

    app js cleanup

--- src/App.js
+++ src/App.js
@@ -1,55 +1,19 @@
 import './App.css';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route 
 } from 'react-router-dom'
 
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Portfolio from './pages/Portfolio';
+import Home from './pages/index'
 
 function App() {
   return (
-
     <Router>
-      <div>
-        <nav>
-          <Link to='/'>
-            Home
-          </Link>
-          <br />
-          <Link to='/about'>
-            About
-          </Link>
-          <br />
-          <Link to='/contact'>
-            Contact
-          </Link>
-          <br />
-          <Link to='/portfolio'>
-            Portfolio
-          </Link>
-        </nav>
-        <Switch>
-          <Route exact path="/">
-            <h1>HOME PAGE</h1>
-          </Route>
-          <Route path='/about'> 
-            <About />
-          </Route>
-          <Route path='/contact'> 
-            <Contact />
-          </Route>
-          <Route path='/portfolio'> 
-            <Portfolio />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
     </Router>
-
   );
 }
 
