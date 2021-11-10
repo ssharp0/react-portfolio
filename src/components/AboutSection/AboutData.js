823ef95f2a3939fc5b316fe9f56dc445a21d8b77
SHARP <ssharp9999@gmail.com>
2021-11-09 17:53:25 -0800

    added desc

--- src/components/AboutSection/AboutData.js
+++ src/components/AboutSection/AboutData.js
@@ -1,4 +1,6 @@
 export const aboutData = {
  heading: 'About Me',
- aboutDesc: 'I am a Southern California resident and love to go hiking with my partner Emily and my two amazing pups. I have a team oriented personality and love working with others to build exciting applications and implement new technologies'
+ aboutDesc: 'I am a Southern California resident and love to go hiking and camping with my two amazing pups and family.',
+ aboutDesc2: 'I have a passion for traveling, and in my free time I enjoy watching sports and skiing on a fresh powder day.',
+ aboutDesc3: 'I have a team oriented personality and love working with others to build exciting applications and implement new technologies.'
 }
