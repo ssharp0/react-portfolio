5438645b6eaecfeaf933505d13042ed852761bde
SHARP <ssharp9999@gmail.com>
2021-11-09 17:54:18 -0800

    heading

--- src/components/SidebarNav/SidebarNavElements.js
+++ src/components/SidebarNav/SidebarNavElements.js
@@ -1,6 +1,23 @@
 import styled from "styled-components";
 import { Link as LinkScroll } from 'react-scroll'
 import { FaWindowClose } from 'react-icons/fa'
+import { Paper } from '@mui/material'
+
+export const SideNavHeaderPaper = styled(Paper)`
+ margin-left: 20%;
+ margin-right: 20%;
+ align-self: center;
+ cursor: default;
+`
+
+export const SideNavHeader = styled.h1`
+ color: #A56336;
+ font-size: 2.5rem;
+
+ @media screen and (max-width: 480px) {
+  font-size: 1.5rem;
+ }
+`
 
 export const SideNavContainer = styled.aside`
  background: #143642;
@@ -21,32 +38,31 @@ export const IconDiv = styled.div`
  top: 1.5rem;
  right: 1.5rem;
  position: absolute;
- background: transparent;
  font-size: 2rem;
  cursor: pointer;
- outline: none;
  
   &:hover {
   font-size: 2.5rem;
+  color: #A56336;
  }
 `
 
 export const SideNavCloseIcon = styled(FaWindowClose)`
  color: #fff;
+
+  &:hover {
+  color: #A56336;
+ }
 `
 
-export const SideNavWrapper = styled.div`
- color: #fff;
-`
-
-export const SideNavMenu = styled.ul`
+export const SideNavMenu = styled.li`
  display: grid;
  grid-template-column: 1fr;
- grid-template-rows: repeat(4, 75px);
+ grid-template-rows: repeat(5, 75px);
  text-align: center;
 
  @media screen and (max-width: 480px) {
-  grid-template-rows: repeat(4, 55px);
+  grid-template-rows: repeat(5, 55px);
  }
 `
 
@@ -55,9 +71,6 @@ export const SideNavItem = styled(LinkScroll)`
  justify-content: center;
  display: flex;
  font-size: 1.5rem;
- text-decoration: none;
- list-style: none;
- text-decoration: none;
  color: #fff;
  cursor: pointer;
  transition: 0.5s ease-in-out;
@@ -67,11 +80,7 @@ export const SideNavItem = styled(LinkScroll)`
   font-size: 2rem;
   transition: 0.3s ease-in-out;
   border-bottom: 1px solid #fff;
-  border-top: 1px solid #fff;
+  margin-left: 20%;
+  margin-right: 20%;
  }
-`
-
-export const SideNavBtnWrap = styled.div`
- display: flex;
- justify-content: center
 `
